57aa010e92ed8821f184bc87cd792b3e872fe9e3
James Perkins <james@unkey.dev>
2024-06-18 08:09:24 -0400

    update

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -1,17 +1,9 @@
 import * as OpenAPI from "fumadocs-openapi";
-import * as Typescript from "fumadocs-typescript";
-import * as path from "node:path";
 
 void OpenAPI.generateFiles({
   input: ["*.json"],
-  output: "../content/docs/ui",
-});
-
-void Typescript.generateFiles({
-  input: ["./content/docs/**/*.model.mdx"],
-  output: (file) =>
-    path.resolve(
-      path.dirname(file),
-      `${path.basename(file).split(".")[0]}.mdx`,
-    ),
-});
+  output: "../content/docs/",
+  per: "tag",
+})
+  .catch(console.error)
+  .then((e) => console.log(e));
