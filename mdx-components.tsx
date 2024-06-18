bc686f8617fdb3be5cc42289b7c02dd743f10959
James Perkins <james@unkey.dev>
2024-06-18 08:11:55 -0400

    remove mdx-component

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,13 +2,11 @@ import type { MDXComponents } from "mdx/types";
 import defaultComponents from "fumadocs-ui/mdx";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import { Callout } from "fumadocs-ui/components/callout";
-import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     Tab,
     Tabs,
     ...defaultComponents,
-    img: (props) => <ImageZoom {...props} />,
     Callout,
     ...components,
   };
