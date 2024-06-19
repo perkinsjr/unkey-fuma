e821e89f788aa7968fa44b56f0472e71a16e8dae
James Perkins <james@unkey.dev>
2024-06-18 22:18:21 -0400

    yeet

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,14 @@
 import type { MDXComponents } from "mdx/types";
 import defaultComponents from "fumadocs-ui/mdx";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { Steps, Step } from "fumadocs-ui/components/steps";
 import { Callout } from "fumadocs-ui/components/callout";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     Tab,
     Tabs,
+    Steps,
+    Step,
     ...defaultComponents,
     Callout,
     ...components,
